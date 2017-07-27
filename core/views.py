b02cc68df831978be4d46f254efba8f3d7ba963e
zhonghua <zhonghua.wang@outlook.com>
2017-07-27 18:57:55 +0800

    init permission 7-27

--- core/views.py
+++ core/views.py
@@ -3,9 +3,9 @@ from __future__ import unicode_literals
 
 from rest_framework import viewsets
 from . import serializer, models
-
+from rest_framework import permissions
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = models.User.objects.all()
     serializer_class = serializer.UserSerializer
-
+    permission_classes = (permissions.IsAuthenticated, )
--- instrument/views.py
+++ instrument/views.py
@@ -7,35 +7,38 @@ from django_filters import filters
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import permissions
 from .permissions import IsOwnerOrReadOnly
-# Create your views here.
 
 
 class DepartmentViewSet(viewsets.ModelViewSet):
     queryset = models.Department.objects.all()
     serializer_class = seriazlizers.DepartmentSerializer
+    permission_classes = (permissions.DjangoModelPermissionsOrAnonReadOnly,)
 
 
 class ManufacturerViewSet(viewsets.ModelViewSet):
     queryset = models.Manufacturer.objects.all()
     serializer_class = seriazlizers.ManufacturerSerializer
+    permission_classes = (permissions.DjangoModelPermissionsOrAnonReadOnly,)
 
 
 class ReservationTypeViewSet(viewsets.ModelViewSet):
     queryset = models.ReservationType.objects.all()
     serializer_class = seriazlizers.ReservationTypeSerializer
+    permission_classes = (permissions.DjangoModelPermissionsOrAnonReadOnly,)
 
 
 class InstrumentViewSet(viewsets.ModelViewSet):
     queryset = models.Instrument.objects.all()
     serializer_class = seriazlizers.InstrumentSerializer
-    filter_backends = (DjangoFilterBackend, )
+    filter_backends = (DjangoFilterBackend,)
     filter_fields = ('department', 'charge_type', 'status', 'manufacturer')
+    permission_classes = (permissions.DjangoModelPermissionsOrAnonReadOnly,)
 
 
 class ReservationViewSet(viewsets.ModelViewSet):
     queryset = models.Reservation.objects.all()
-    filter_backends = (DjangoFilterBackend, )
-    filter_fields = ('instrument', )
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ('instrument',)
     serializer_class = seriazlizers.ReservationSerializer
 
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
